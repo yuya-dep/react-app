e66267b786ccbef95adf1d7ce41bf0c9175d7b0e
Yuya.Takayanagi <yuyatakayanagi@cock.email>
2021-03-19 10:29:09 +0900

    router,materialUI導入

---
+++ app/src/components/tutorial/GameBoardSquareComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+    value: number
+}
+
+export class GameBoardSquareComponent extends React.Component{
+  render() {
+    return (
+      <button className="square">
+        {/* {this.props.value} */}
+      </button>
+    );
+  }
+}
